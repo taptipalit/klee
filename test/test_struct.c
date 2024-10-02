7cd43f0e5f00fde2b20fda6e457176d711ca5152
Tapti Palit <tpalit@purdue.edu>
2024-10-02 13:14:12 -0700

    New test

---
+++ test/test_struct.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <klee/klee.h>
+
+struct Student {
+	int id;
+	int age;
+	char* name;
+};
+
+
+void function(struct Student* s) {
+	if (s->id > 10) {
+		if (s->age < 20) {
+			printf("Welcome %s!!!\n", s->name);
+		} else {
+			printf("Welcome %s!!\n", s->name);
+		}
+	}
+}
+
+
+int main(void) {
+	// Fix this now
+	struct Student s;
+	/*
+	int* p, *q;
+	p = &(s.id);
+	q = &(s.age);
+	*/
+	s.name = malloc(100);
+	/*
+	klee_make_symbolic(p, sizeof(int), "id");
+	klee_make_symbolic(q, sizeof(int), "age");
+	*/
+	klee_make_symbolic(&s, sizeof(s), "student");
+
+	klee_make_symbolic(s.name, 100, "name");
+
+	function(&s);
+}
