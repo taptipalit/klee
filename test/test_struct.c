43d729cc66a58f7c87d92fa36eea98023873cdbb
Tapti Palit <tpalit@purdue.edu>
2024-10-11 18:25:54 -0700

    change test case

--- test/test_struct.c
+++ test/test_struct.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <klee/klee.h>
 
+
 struct Student {
 	int id;
 	int age;
@@ -8,7 +9,11 @@ struct Student {
 };
 
 
-void function(struct Student* s) {
+void function(struct Student* s, int* b) {
+	int a = s->age;
+	s->id = a + 20;
+	*b = 300;
+	/*
 	if (s->id > 10) {
 		if (s->age < 20) {
 			printf("Welcome %s!!!\n", s->name);
@@ -16,6 +21,7 @@ void function(struct Student* s) {
 			printf("Welcome %s!!\n", s->name);
 		}
 	}
+	*/
 }
 
 
@@ -35,6 +41,12 @@ int main(void) {
 	klee_make_symbolic(&s, sizeof(s), "student");
 
 	klee_make_symbolic(s.name, 100, "name");
+	int b;
+	klee_make_symbolic(&b, sizeof(b), "b");
 
-	function(&s);
+	function(&s, &b);
+	klee_print_expr("Symbolic value of s.id:", (s.id));
+	klee_print_expr("Symbolic value of s.age: ", (s.age));
+
+	klee_print_expr("Symbolic value of b: ", b);
 }
